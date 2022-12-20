73ce5e420e2993394735252bb0001da599ef4b00
Nipuni-Jayathilaka <nipunipavithrajayathilaka@gmail.com>
2022-12-20 14:10:30 +0530

    Fallback to JDBC

--- src/main/java/lk/ijse/dep9/entity/Project.java
+++ src/main/java/lk/ijse/dep9/entity/Project.java
@@ -6,29 +6,23 @@ import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-@ToString(exclude = "taskSet")
-@EqualsAndHashCode(exclude = "taskSet")
-@Entity
+//@ToString(exclude = "taskSet")
+//@EqualsAndHashCode(exclude = "taskSet")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Project implements SuperEntity{
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column(nullable = false)
     private String name;
-    @ManyToOne
-    @JoinColumn(name = "username",referencedColumnName = "username")
-    private User user;
-    @Setter(AccessLevel.NONE)
-    @OneToMany(mappedBy = "project", fetch = FetchType.EAGER, cascade = {CascadeType.REMOVE})//when remove
-    // the project remove the related task list automatically, if use all without using remove then all the
-    // things we done will happen to all the tasks cascaded with the projects
-    private Set<Task> taskSet = new HashSet<>();
+    private String username;
+//    @Setter(AccessLevel.NONE)
+////    @OneToMany(mappedBy = "project", fetch = FetchType.EAGER, cascade = {CascadeType.REMOVE})//when remove
+////    // the project remove the related task list automatically, if use all without using remove then all the
+//    // things we done will happen to all the tasks cascaded with the projects
+//    private Set<Task> taskSet = new HashSet<>();
 
-    public Project(String name, User user) {
+    public Project(String name, String username) {
         this.name = name;
-        this.user = user;
+        this.username = username;
     }
 }
--- src/main/java/lk/ijse/dep9/entity/User.java
+++ src/main/java/lk/ijse/dep9/entity/User.java
@@ -8,15 +8,12 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User implements SuperEntity{
-    @Id
     private String username;
-    @Column(name = "full_name", nullable = false)
     private String fullName;
-    @Column(nullable = false)
     private String password;
 }
