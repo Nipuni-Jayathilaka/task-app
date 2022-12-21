0b51082dad5f1f823050bfc82579778c9e53005f
Nipuni-Jayathilaka <nipunipavithrajayathilaka@gmail.com>
2022-12-21 11:50:07 +0530

    Handle validation exception smoothly

---
+++ src/main/java/lk/ijse/dep9/app/advice/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package lk.ijse.dep9.app.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String , Object> validationException(MethodArgumentNotValidException e){
+        HashMap<String , Object> errAttribute = new LinkedHashMap<>();
+        errAttribute.put("status",HttpStatus.BAD_REQUEST.value());
+        errAttribute.put("error",HttpStatus.BAD_REQUEST.getReasonPhrase());
+        errAttribute.put("timestamp",new Date().toString());
+        List<String> validationErrList = e.getFieldErrors().stream().map(err -> err.getField() + ":" + err.getDefaultMessage()).collect(Collectors.toList());
+        errAttribute.put("errors",validationErrList);
+        return errAttribute;
+    }
+}
+
--- src/main/java/lk/ijse/dep9/app/dto/UserDTO.java
+++ src/main/java/lk/ijse/dep9/app/dto/UserDTO.java
@@ -3,14 +3,23 @@ package lk.ijse.dep9.app.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO implements Serializable {
+    @NotBlank(message = "Full name can't be empty or null")
+    @Pattern(regexp = "^[a-zA-Z ]+$",message = "Invalid name")
     private String fullName;
+    @NotBlank (message = "Username can't be empty or null")
     private String username;
+    @NotEmpty(message = "Password can't be empty or null")
+    @Length(min=3, message = "Password should be at least 3 character long")
     private String password;
 }
