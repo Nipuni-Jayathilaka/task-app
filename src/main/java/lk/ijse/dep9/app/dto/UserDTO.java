a6231885d981cea9bfbebeffff00369e408b2259
Nipuni-Jayathilaka <nipunipavithrajayathilaka@gmail.com>
2022-12-21 12:08:43 +0530

    Finalize the UserDTO

--- src/main/java/lk/ijse/dep9/app/dto/UserDTO.java
+++ src/main/java/lk/ijse/dep9/app/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package lk.ijse.dep9.app.dto;
 
+import lk.ijse.dep9.app.util.ValidationGroups;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import org.hibernate.validator.constraints.Length;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraintvalidation.ValidationTarget;
 import java.io.Serializable;
 
 @Data
@@ -17,7 +19,7 @@ public class UserDTO implements Serializable {
     @NotBlank(message = "Full name can't be empty or null")
     @Pattern(regexp = "^[a-zA-Z ]+$",message = "Invalid name")
     private String fullName;
-    @NotBlank (message = "Username can't be empty or null")
+    @NotBlank (message = "Username can't be empty or null", groups = ValidationGroups.Create.class)
     private String username;
     @NotEmpty(message = "Password can't be empty or null")
     @Length(min=3, message = "Password should be at least 3 character long")
